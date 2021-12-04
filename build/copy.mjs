524c42df7d0f7556cce1246874aca74e2cc7281c
sapphi-red <green@sapphi.red>
2021-12-04 21:39:43 +0900

    Chore: format build/copy.mjs

--- build/copy.mjs
+++ build/copy.mjs
@@ -3,7 +3,9 @@ import fg from 'fast-glob'
 import path from 'path'
 
 const modelFiles = await fg('./DTLN/pretrained_model/*.tflite')
-await Promise.all(modelFiles.map(modelFile => {
-  const filename = path.basename(modelFile)
-  return fs.copyFile(modelFile, path.join('dist/', filename))
-}))
+await Promise.all(
+  modelFiles.map(modelFile => {
+    const filename = path.basename(modelFile)
+    return fs.copyFile(modelFile, path.join('dist/', filename))
+  })
+)
